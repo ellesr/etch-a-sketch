a661a3fae52ff2a2823baae7d855149d3314702e
ellesr <116410947+ellesr@users.noreply.github.com>
2025-03-06 02:43:40 -0800

    Create resetBoard function and call on click

--- javascript.js
+++ javascript.js
@@ -48,3 +48,8 @@ function colorDiv() {
 function setColor(colorChoice) {
     color = colorChoice;
 }
+
+function resetBoard() {
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+}
