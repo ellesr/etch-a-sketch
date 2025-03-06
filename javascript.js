52515635bcd3edb13574eb613ecb70ba39487332
ellesr <116410947+ellesr@users.noreply.github.com>
2025-03-06 02:02:08 -0800

    Add getSize function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     createBoard(32);
+    getSize();
 });
 
 function createBoard(size) {
@@ -13,5 +14,16 @@ function createBoard(size) {
     for (let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
         board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize() {
+    let input = prompt("Enter sketchpad size between 0 and 100:");
+    if (input == "") {
+        alert("Please enter a number.");
+    } else if (input < 0 || input > 100) {
+        alert("Please enter a number between 0 and 100.");
+    } else {
+        alert("Now you play!");
     }
 }
